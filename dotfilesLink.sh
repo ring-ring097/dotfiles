07ff373720ce7cd21b8abfdb67d49d3d482a36b9
rtosa <rtosa@lycorp.co.jp>
2025-06-07 23:48:18 +0900

    add obsidian settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,6 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
- ln -sf ~/dotfiles/coc ~/.config/coc
+ln -sf ~/dotfiles/coc ~/.config/coc
 ln -sf ~/dotfiles/karabiner ~/.config/karabiner
 
